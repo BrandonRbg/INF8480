2058c10743b72e4de3b4027f76596a2c3cd91fa7
Brandon Roberge <brandonroberge@hotmail.com>
2018-12-03 22:27:41 -0500

    Script

--- TP3/script.js
+++ TP3/script.js
@@ -3,11 +3,16 @@ const fetch = require("node-fetch");
 (async function() {
     const time = Date.now();
     const results = (await Promise.all(
-        new Array(30).fill(0).map(
-            async () => fetch("http://132.207.12.213:8080/")
-            .then(r => r.text())
-            .then(res => console.log(res))
-        )
+        new Array(30).fill().map(async () => {
+            try {
+                const beginTime = Date.now();
+                const r = await fetch("http://132.207.12.217:8080/");
+                console.log(`Temps de la requête: ${(Date.now() - beginTime) / 1000} secondes.`)
+                console.log(await r.text());
+            } catch(e) {
+                console.log("Échec de la requête.")
+            }
+        })
     ));
     
     console.log(`Total time: ${(Date.now() - time) / 1000} secondes.`);
