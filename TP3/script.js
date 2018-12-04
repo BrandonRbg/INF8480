ff1bd1037ea92debb3507f18996aae7d57750f85
Simon Lacasse <silace@l4714-12.info.polymtl.ca>
2018-12-03 23:34:49 -0500

    deploiement

--- TP3/script.js
+++ TP3/script.js
@@ -5,7 +5,7 @@ const fetch = require("node-fetch");
     const results = (await Promise.all(
         new Array(30).fill().map(async () => {
             const beginTime = Date.now();
-            const r = await fetch("http://132.207.12.217:8080/")
+            const r = await fetch("http://132.207.12.220:8080/")
                 .catch((err) => console.log(err.message));
             console.log(`Temps de la requête: ${(Date.now() - beginTime) / 1000} secondes.`)
             console.log(await r.text());
